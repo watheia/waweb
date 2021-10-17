4632b6d2e4319d84a6cc61e1108ffe686221e093
Aaron R Miller <amiller@watheia.org>
2021-10-17 03:08:12 -0700

    import css in ssr mode
    Signed-off-by: Aaron R Miller <amiller@watheia.org>

--- apps/web/gatsby-ssr.js
+++ apps/web/gatsby-ssr.js
@@ -6,6 +6,7 @@
 
 import React from "react"
 import Layout from "./src/components/Layout"
+import "./src/styles/index.css"
 
 export const wrapPageElement = ({ element, props }) => {
   // props provide same data to Layout as Page element will get
