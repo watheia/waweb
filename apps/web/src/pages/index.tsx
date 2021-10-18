7e737082f92c03465527965626d782aa24304c42
Aaron R Miller <amiller@watheia.org>
2021-10-17 20:44:53 -0700

    Update hero text
    Signed-off-by: Aaron R Miller <amiller@watheia.org>

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -117,12 +117,13 @@ const HeroSection = () => (
         </div>
         <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
           <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-            <span className="block text-white">Take control of your</span>
-            <span className="block text-teal-200">customer support</span>
+            <span className="block text-white">Build products for the</span>
+            <span className="block text-teal-200">modern web</span>
           </h1>
           <p className="mt-6 max-w-lg mx-auto text-center text-xl text-teal-200 sm:max-w-3xl">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat
-            commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+            Get hands on experience and personilzed training from the best and brightest while
+            contributing to real Open Source Software (OSS) projects used every day by companies
+            like Google, Facebook, Amazon, Apple, and more!
           </p>
           <div className="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
             <div className="space-y-4 sm:space-y-0 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5">
@@ -130,7 +131,7 @@ const HeroSection = () => (
                 href="#"
                 className="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-teal-700 bg-white hover:bg-teal-200 sm:px-8"
               >
-                Contact Sales
+                Join Waitlist
               </a>
               <a
                 href="#"
