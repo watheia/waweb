32043c40a1b066c00587377b509981c8a811ef63
Aaron R Miller <amiller@watheia.org>
2021-10-17 18:04:40 -0700

    Update alert banner
    Signed-off-by: Aaron R Miller <amiller@watheia.org>

--- apps/web/src/components/alert.tsx
+++ apps/web/src/components/alert.tsx
@@ -1,42 +1,55 @@
-import Container from "./container"
-import cn from "clsx"
-import { EXAMPLE_PATH } from "../constants"
+import React, { useState } from "react"
+import { XIcon } from '@heroicons/react/outline'
+import { HtmlHTMLAttributes } from "react"
 
-export default function Alert({ preview }) {
-  return (
-    <div
-      className={cn("border-b", {
-        "bg-accent-7 border-accent-7 text-white": preview,
-        "bg-accent-1 border-accent-2": !preview
-      })}
-    >
-      <Container>
-        <div className="py-2 text-center text-sm">
-          {preview ? (
-            <>
-              This is page is a preview.{" "}
-              <a
-                href="/api/exit-preview"
-                className="underline hover:text-cyan duration-200 transition-colors"
-              >
-                Click here
-              </a>{" "}
-              to exit preview mode.
-            </>
-          ) : (
-            <>
-              The source code for this blog is{" "}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-success duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
-            </>
-          )}
+export type AlertProps = { preview?: boolean } & HtmlHTMLAttributes<HTMLDivElement>
+
+const AlertContent = ({ preview }) => (
+  <p className="text-white">
+    {preview ? (
+      <>
+        This is page is a preview.{" "}
+        <a
+          href="/api/exit-preview"
+          className="underline hover:text-cyan duration-200 transition-colors"
+        >
+          Click here
+        </a>{" "}
+        to exit preview mode.
+      </>
+    ) : (
+      <>
+        The source code for this site is{" "}
+        <span className="block sm:ml-2 sm:inline-block">
+          <a href="https://github.com/watheia/waweb" className="text-white font-medium underline"
+            target="__blank">
+            {' '}
+            available on GitHub <span aria-hidden="true">&rarr;</span>
+          </a>
+        </span>
+      </>
+    )}
+  </p>
+)
+export default function Alert({ preview }: AlertProps) {
+  const [visible, setVisible] = useState<boolean>(true)
+  return visible ? (
+    <div className="relative bg-teal-600">
+      <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+        <div className="pr-16 sm:text-center sm:px-16">
+          <AlertContent preview={preview} />
         </div>
-      </Container>
+        <div className="absolute inset-y-0 right-0 pt-1 pr-1 flex items-start sm:pt-1 sm:pr-2 sm:items-start">
+          <button
+            type="button"
+            className="flex p-2 rounded-md hover:bg-teal-500 focus:outline-none focus:ring-2 focus:ring-white"
+            onClick={() => setVisible(false)}
+          >
+            <span className="sr-only">Dismiss</span>
+            <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  ) : null
 }
